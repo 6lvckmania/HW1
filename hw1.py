9f332cd652dc4aaf1db5498d641f268683faf2de
Yevhenii Tymofeiev <54776811+6lvckmania@users.noreply.github.com>
2023-01-08 22:52:33 +0200

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,9 +1,9 @@
 import os, sys
 
 path = os.path.join(os.path.expanduser('~'), sys.argv[1])
-prefix = sys.argv[2]
-counts = sys.argv[3]
-mode = sys.argv[4]
+prefix = sys.argv[2] #folder name
+counts = sys.argv[3] #counts of users
+mode = sys.argv[4] #permisions mode
 
 for i in range (1, int(counts) + 1):
     usrpath = os.path.join(path, prefix + str(i))
